a3f8132ac6775a46904aab9fd1f92896b4d88e97
AshuraNET <christopher080801@web.de>
2023-01-18 10:32:50 +0100

    WIP

--- footer.php
+++
@@ -1,8 +0,0 @@
-<div id="footer">
-    Version <?php echo DB_VERSION ?>
-</div>
-
-<script type="text/javascript">
-    // Überlappung vom Body mit Footer verhindern
-    document.getElementById("main-div").style.marginBottom = document.getElementById("footer").clientHeight + "px";
-</script>
