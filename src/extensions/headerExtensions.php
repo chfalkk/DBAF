b0a8cf6c527d6ced63bc7aa34f251a14549c10b2
AshuraNET <christopher080801@web.de>
2023-01-18 10:27:14 +0100

    Finieshed Status Anziege

--- src/extensions/headerExtensions.php
+++ src/extensions/headerExtensions.php
@@ -6,7 +6,7 @@
     class HeaderExtensions{
 
         /**
-         * 
+         * @return String Einen HTML-Codierten String um den API-Status darzustellen
          */
         public static function DisplayAPIStatus(){
             $StaDaStatus = DBAPI_StaDa::StaDaIsAvailable();
@@ -15,6 +15,24 @@
 
             $StaDaIcon = ($StaDaStatus) ? IconRessources::$APIAvailable : IconRessources::$APINotAvailable;
             $FahrplanStatusIcon = ($FahrplanAPIStatus) ? IconRessources::$APIAvailable : IconRessources::$APINotAvailable;
+
+            $StaDaPillClass = ($StaDaStatus) ? "success" : "warning";
+            $FahrplanPillClass = ($StaDaStatus) ? "success" : "warning";
+
+            $PillDescription = " In diesem Element sehen Sie den Status der Bahn-API. \n
+            ROT bedeutet, dass die API nicht erreichbar ist. Dementsprechend können einige Kernfunktionalitäten der Anwendung nicht benutzt werden!\n
+            GRÜN bedeutet, dass die API erreichbar ist. Alle Funktionalitäten können wie erwartet benutzt werden.";
+
+
+            
+        $res = "<span class=\"d-inline-block\" tabindex=\"0\" data-toggle=\"tooltip\" title=\"Disabled tooltip\">
+                        <button class=\"badge badge-pill badge-".$StaDaPillClass."\" style=\"pointer-events: none;\" type=\"button\" disabled>".$StaDaIcon . $PillDescription."</button>
+                    </span>";
+        $res .= "<span class=\"d-inline-block\" tabindex=\"0\" data-toggle=\"tooltip\" title=\"Disabled tooltip\">
+                        <button class=\"badge badge-pill badge-".$FahrplanPillClass."\" style=\"pointer-events: none;\" type=\"button\" disabled>".$FahrplanStatusIcon . $PillDescription."</button>
+                    </span>";
+
+            
         }
 
     }
