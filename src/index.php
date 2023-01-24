8d9fc52b2979010162d50073fca069ac764a2499
chfalkk <christopher080801@web.de>
2023-01-24 20:49:40 +0100

    Added Status-Abfrage zur NAVBAR + Toggler

--- src/index.php
+++ src/index.php
@@ -15,6 +15,10 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
         <link rel="stylesheet" href="css/style.css">
         <link rel="icon" href="img/favicon.ico">
     </head>
@@ -24,24 +28,9 @@
         ?>
         <div id="main-div">
             <?php
-            /*
-                $query = new DBAPI_StaDa();
+            
+                
 
-                echo 'Fahrplan: ' . DBAPI_Fahrplan::FahrplanIsAvailable();
-                echo 'StaDa: ' . DBAPI_StaDa::StaDaIsAvailable();
-
-                $filter = new StationFilter();
-                $filter->limit = 100;
-                $query->GetStations($filter);
-                //$query->GetArrivalBoard(8011201, '2023-01-30');
-
-                //if ($query->GetErrorMessage() != '') {
-                //    echo $query->GetErrorMessage();
-                //} else {
-                    $result = $query->GetJSONResult();
-                    echo TableBuilder::BuildTable('Locations', $result);
-                //} 
-            */
             ?>
         </div>
 
--- src/ressources/iconRessources.php
+++ src/ressources/iconRessources.php
@@ -1,5 +1,8 @@
 <?php
     class IconRessources {
-        public static string $APINotAvailable = "<i class=\"fa-solid fa-clipboard-question\"></i>";
-        public static string $APIAvailable = "<i class=\"fa-solid fa-clipboard-check\"></i>";
+        public static string $APINotAvailable = "<i class=\"fa-regular fa-circle-xmark dbaf-pill-icon\"></i>";
+        public static string $APIAvailable = "<i class=\"fa-regular fa-circle-check dbaf-pill-icon\"></i>";
+
+
+        public static string $Information = "<i class=\"fa-solid fa-circle-info\"></i>";
     }
