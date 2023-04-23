b74e7f1be0aed3872551f085d619411345a66001
oli-a38p <oliverholmquist2014@hotmail.com>
2023-04-23 21:02:26 +0200

    Alte Daten entfernt, Aufräumen der Verzeichnisstruktur
    - alte Daten (PHP) entfernt
    - aktuelle Dateien aufgeräumt und umstrukturiert
    - Arbeitsnachweis nachträglich aktualisiert
    
    index.html:
    - Design für Layout-Boxen fertiggestellt

--- src/partials/footer.php
+++
@@ -1,8 +0,0 @@
-<footer id="footer">
-        Version <?php echo DB_VERSION ?>
-</footer>
-
-<script type="text/javascript">
-    // Überlappung vom Body mit Footer verhindern
-    document.getElementById("dbaf-main-div").style.marginBottom = document.getElementById("footer").clientHeight + "px";
-</script>
